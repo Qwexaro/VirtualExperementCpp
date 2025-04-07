30c3f6a9c61173ac597ad1f06b5817c6d7d70e18
Qwexaro <Sergikstar25.gold@mail.ru>
2025-04-07 11:52:00 +0300

    main

---
+++ VirtualExperement/RPO1_IvanovSergey_virtual.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+class Old 
+{
+public:
+	virtual void info() { std::cout << "‗ מכה\n"; }
+};
+
+class Nouveau : public Old 
+{
+public:
+	void info() { std::cout << "‗ םו מכה\n"; }
+};
+
+int main()
+{
+	system("chcp 1251");
+	Old* old_ptr = new Nouveau;
+	old_ptr->info();
+	return 0;
+}
